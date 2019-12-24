cfb45f4af04f52e42f8066bcda7164579f034fe3
Gero <geropellicer@gmail.comm>
2019-12-23 21:11:59 -0300

    Implemented router and routes to login and register

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import Register from './components/Auth/register';
+import Login from './components/Auth/login';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'semantic-ui-css/semantic.min.css';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route path="/" exact component={App} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/register" exact component={Register} />
+        </Switch>
+    </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
