dee2970dfdec4db0ff2799c751a1eae53eebb076
Gero <geronimo@devsar.com>
2020-01-08 11:32:55 -0300

    Added authentication rediretion

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-import Register from './components/Auth/register';
-import Login from './components/Auth/login';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+import Register from "./components/Auth/register";
+import Login from "./components/Auth/login";
+import firebase from './firebase';
 
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
-const Root = () => (
+const Root = () => {
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        return(
+          <Redirect to="/"/>
+        );
+      }
+    });
+  }, []);
+
+  return (
     <Router>
-        <Switch>
-            <Route path="/" exact component={App} />
-            <Route path="/login" exact component={Login} />
-            <Route path="/register" exact component={Register} />
-        </Switch>
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+      </Switch>
     </Router>
-);
+  );
+};
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById("root"));
 registerServiceWorker();
