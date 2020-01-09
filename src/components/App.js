8bcdbfd47ee5be1ee8259f7ab40f87f7da108b5a
Gero <geronimo@devsar.com>
2020-01-09 11:00:29 -0300

    Added basic app structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        App
-      </div>
-    );
-  }
-}
+import ColorPanel from "./colorPanel";
+import SidePanel from "./sidePanel";
+import Messages from "./messages";
+import MetaPanel from "./metaPanel";
+
+const App = () => {
+  return (
+    <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+      <ColorPanel />
+      <SidePanel />
+      <Grid.Column>
+        <Messages style={{ marginLeft: 320 }} />
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <MetaPanel />
+      </Grid.Column>
+    </Grid>
+  );
+};
 
 export default App;
