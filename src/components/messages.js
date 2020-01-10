9ad8f9b9bf5d375ed47f2376533621130f8014b7
Gero <geronimo@devsar.com>
2020-01-10 11:03:07 -0300

    Added fetch and display messages in channels

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,18 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Segment, Comment } from "semantic-ui-react";
 import MessagesHeader from "./messagesHeader";
 import MessageForm from "./messageForm";
+import Message from "./message";
+import { useSelector } from "react-redux";
+import firebase from "../firebase";
 
 const Messages = () => {
+  const [messagesRef] = useState(firebase.database().ref("messages"));
+
+  const [userInfo] = useState(useSelector(state => state.user.currentUser));
+  const activeChannel = useSelector(state => state.channels.currentChannel);
+
+  const [messages, setMessages] = useState([]);
+  const [loadingMessages, setLoadingMessages] = useState(true);
+
+  const displayMessages = messages => {
+    messages.length > 0 &&
+      messages.map(message => (
+        <Message key={message.timestamp} message={message} user={userInfo} />
+      ));
+  };
+
+  useEffect(() => {
+    const addMessageListener = channelId => {
+      messagesRef.child(channelId).on("child_added", snap => {
+        setMessages(prevMessages => [...prevMessages, snap.val()]);
+        setLoadingMessages(false);
+      });
+    };
+
+    const addListeners = channelId => {
+      addMessageListener(channelId);
+    };
+
+    if (activeChannel && userInfo) {
+      addListeners(activeChannel.id);
+    }
+  }, [activeChannel, userInfo, messagesRef]);
+
   return (
     <div style={{ marginLeft: 320 }}>
       <MessagesHeader />
 
       <Segment>
-        <Comment.Group className="messages">{/** Messages */}</Comment.Group>
+        <Comment.Group className="messages">
+          {messages.length > 0 ? messages.map(message => (
+            <Message
+              key={message.timestamp}
+              message={message}
+              user={userInfo}
+            />
+          )) : null}
+        </Comment.Group>
       </Segment>
 
-      <MessageForm/>
+      <MessageForm />
     </div>
   );
 };
