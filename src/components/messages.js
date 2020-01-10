648bb8308ab72d4443c5902e44fbcd888d718743
Gero <geronimo@devsar.com>
2020-01-10 09:52:15 -0300

    Added basic messages structure and components

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Segment, Comment } from "semantic-ui-react";
+import MessagesHeader from "./messagesHeader";
+import MessageForm from "./messageForm";
 
 const Messages = () => {
-    return(
-        <div>Messages</div>
-    );
+  return (
+    <div style={{ marginLeft: 320 }}>
+      <MessagesHeader />
+
+      <Segment>
+        <Comment.Group className="messages">{/** Messages */}</Comment.Group>
+      </Segment>
+
+      <MessageForm/>
+    </div>
+  );
 };
 
 export default Messages;
