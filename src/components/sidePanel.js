1a71806271d4f3659f689988ac0a0142d5ae7d23
Gero <geronimo@devsar.com>
2020-01-14 14:12:32 -0300

    Added direct messages component

--- src/components/sidePanel.js
+++ src/components/sidePanel.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Menu } from 'semantic-ui-react';
 import UserPanel from './userPanel';
 import Channels from './channels';
+import DirectMessages from './directMessages';
 
 const SidePanel = () => {
     return(
         <Menu size="large" inverted fixed="left" vertical style={{background: '#4c3c4c', fontSize: '1.2rem'}}>
             <UserPanel/>
             <Channels/>
+            <DirectMessages/>
         </Menu>
     );
 };
