d1b62a35dbe6a0a5b4e8a60c6dfc77b6206dd5c7
Gero <geronimo@devsar.com>
2020-01-14 10:10:48 -0300

    Added count unique users

--- src/components/messages.js
+++ src/components/messages.js
@@ -12,10 +12,26 @@ const Messages = () => {
   const [userInfo] = useState(useSelector(state => state.user.currentUser));
   const activeChannel = useSelector(state => state.channels.currentChannel);
 
+  const [numUniqueUsers, setNumUniqueUsers] = useState("");
+
   const [messages, setMessages] = useState([]);
   const [loadingMessages, setLoadingMessages] = useState(true);
 
+  const displayChannelName = () => {
+    return activeChannel ? activeChannel.name : "";
+  };
+
   useEffect(() => {
+    const countUniqueUsers = () => {
+      const uniqueUsers = messages.reduce((acc, message) => {
+        if (!acc.includes(message.user.name)) {
+          acc.push(message.user.name);
+        }
+        return acc;
+      }, []);
+      setNumUniqueUsers(`${uniqueUsers.length} users`);
+    };
+
     const addMessageListener = channelId => {
       messagesRef.child(channelId).on("child_added", snap => {
         setMessages(prevMessages => [...prevMessages, snap.val()]);
@@ -29,22 +45,28 @@ const Messages = () => {
 
     if (activeChannel && userInfo) {
       addListeners(activeChannel.id);
+      countUniqueUsers();
     }
   }, [activeChannel, userInfo, messagesRef]);
 
   return (
     <div style={{ marginLeft: 320 }}>
-      <MessagesHeader />
+      <MessagesHeader
+        channelName={displayChannelName()}
+        numUniqueUsers={numUniqueUsers}
+      />
 
       <Segment>
         <Comment.Group className="messages">
-          {messages.length > 0 ? messages.map(message => (
-            <Message
-              key={message.timestamp}
-              message={message}
-              user={userInfo}
-            />
-          )) : null}
+          {messages.length > 0
+            ? messages.map(message => (
+                <Message
+                  key={message.timestamp}
+                  message={message}
+                  user={userInfo}
+                />
+              ))
+            : null}
         </Comment.Group>
       </Segment>
 
